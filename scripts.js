60884fb7d31e6e17e34e1fd720b411b10d76bb9a
Bryce Mattson <70603426+PerhapsBryce@users.noreply.github.com>
2024-10-05 14:36:52 -0600

    feat: added parallax picture in pricing

--- scripts.js
+++ scripts.js
@@ -8,24 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
         nav.classList.toggle('show')
     })
-    
+
 
     window.addEventListener('resize', (e) => {
         changeNavSig()
     })
 })
 
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelector('.pricing-pic');
+    if (!parallax) return
+
+    let scrollPosition = window.scrollY;
+
+    parallax.style.transform = `translateY(${scrollPosition * 0.2}px)`;
+});
+
 function changeNavSig() {
     const signature = document.querySelector('.nav-sig')
 
     const urlPrefix = signature.classList.contains('index')
-        ? '.' 
-        : '..' 
+        ? '.'
+        : '..'
 
     if (window.innerWidth < 1024) {
-        signature.src = urlPrefix + '/assets/VerticleSignatures.png'            
+        signature.src = urlPrefix + '/assets/VerticleSignatures.png'
     } else {
         signature.src = urlPrefix + '/assets/signature.png'
     }
 }
- 
