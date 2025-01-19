f4f6da3db28b9307ae2a98ab57bdedff1181c7ca
Bryce Mattson <70603426+PerhapsBryce@users.noreply.github.com>
2025-01-19 13:51:25 -0700

    chore: applied var changed by request

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     changeNavSig()
-
     const navExpand = document.getElementById('nav-expand')
-
     navExpand.addEventListener('click', () => {
         const nav = document.querySelector('nav')
 
         nav.classList.toggle('show')
     })
-
-
     window.addEventListener('resize', (e) => {
         changeNavSig()
+    })
+
+    highlightCurrentSelectedVideoSelector()
+    window.addEventListener("mousedown", ev => {
+        if (!ev?.target || !ev.target.matches(".video-selector")) return
+
+        const selectedVideoIndex = ev.target.dataset.videoid
+        currentVideoId = selectedVideoIndex
+
+        highlightCurrentSelectedVideoSelector()
+        changeVideo(VIDEOS[selectedVideoIndex])
     })
 })
 
@@ -27,4 +34,32 @@ function changeNavSig() {
     } else {
         signature.src = urlPrefix + '/assets/signature.png'
     }
+}
+
+const YOUTUBE_IFRAME_PARAMS = "?rel=0"
+const VIDEOS = [
+    `https://www.youtube.com/embed/_y0UMyyuOS0${YOUTUBE_IFRAME_PARAMS}`,
+    `https://www.youtube.com/embed/6V9zreejkFU${YOUTUBE_IFRAME_PARAMS}`,
+    `https://www.youtube.com/embed/g5i6vgihJZU${YOUTUBE_IFRAME_PARAMS}`,
+    `https://www.youtube.com/embed/wNzd5q0FLuA${YOUTUBE_IFRAME_PARAMS}`,
+    `https://www.youtube.com/embed/XVfn5mpka8I${YOUTUBE_IFRAME_PARAMS}`
+]
+let currentVideoId = 0
+function highlightCurrentSelectedVideoSelector() {
+    const currentHighlightedItem = document.querySelector(".selected-video")
+    if (currentHighlightedItem) {
+        currentHighlightedItem.classList.remove("selected-video")
+    }
+
+    const selectedVideoSelector = document.querySelector(`[data-videoId="${currentVideoId}"]`)
+    if (!selectedVideoSelector) return
+
+    selectedVideoSelector.classList.add("selected-video")
+}
+function changeVideo(src) {
+    const youtubeIframe = document.querySelector('#youtube-iframe')
+    if (!youtubeIframe) return
+
+    youtubeIframe.src = src
+
 }
